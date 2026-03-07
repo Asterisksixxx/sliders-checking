41fd9ffd1c542c7bb219184f96c4db8348d1129d
Ambrazevich Siarhei <heroes51635@mail.ru>
2026-03-07 22:59:30 +0300

    update indicators

--- scripts/slider.js
+++ scripts/slider.js
@@ -6,28 +6,28 @@ const $prevButton = document.getElementById("prev");
 const $nextButton = document.getElementById("next");
 const $indicatorList = document.getElementById("indicators-list");
 
-const slidesCount = 2;
-const slidesToScroll = 1;
+const slidesCount = 3;
+const slidesToScroll = 2;
 const arrayOfIndexSlides = [];
 
-const handlePrevScroll = () => {
+const handleMoveToPrev = () => {
   $sliderList.scrollTo({
     left: getSliderOffset() - getSliderItemWidth() * slidesToScroll,
     behavior: "smooth",
   });
-  setTimeout(() => {
-    getActiveIndicators();
-  }, 650);
+  // setTimeout(() => {
+  //   getActiveIndicators();
+  // }, 650);
 };
 
-const handleNextScroll = () => {
+const handleMoveToNext = () => {
   $sliderList.scrollTo({
     left: getSliderOffset() + getSliderItemWidth() * slidesToScroll,
     behavior: "smooth",
   });
-  setTimeout(() => {
-    getActiveIndicators();
-  }, 650);
+  // setTimeout(() => {
+  //   getActiveIndicators();
+  // }, 650);
 };
 
 const getSliderContainerDimensions = () => {
@@ -44,10 +44,11 @@ const getSliderOffset = () => {
 };
 
 const createIndicators = () => {
-  const items = Array.from($sliderItems);
-  items.forEach((item) => {
-    appendIndicatorsList();
-  });
+  const length = Math.ceil(
+    $sliderItems.length / slidesToScroll - (slidesCount - slidesToScroll),
+  );
+  const items = Array(length).fill(null);
+  items.forEach(appendIndicatorsList);
 };
 
 const appendIndicatorsList = () => {
@@ -60,35 +61,55 @@ const appendIndicatorsList = () => {
                 </li>`,
   );
 };
-const getActiveIndicators = () => {
-  console.log("offset= " + getSliderOffset());
 
-  if (getSliderOffset() !== 0) {
-    console.log("not zero");
-    const lastSlide = Math.round(
-      getSliderOffset() / getSliderItemWidth() + slidesCount,
-    );
-    console.log("last slide= " + lastSlide);
-    const arr = [];
-    for (
-      let i = Math.round(getSliderOffset() / getSliderItemWidth());
-      i < lastSlide;
-      i++
-    ) {
-      arr.push(Math.round(i));
-    }
-    console.log("slides= " + arr);
-    return arr;
-  } else {
-    console.log("zero");
-    const arr = [];
-    for (let i = 0; i < slidesCount; i++) {
-      arr.push(Math.round(i));
-    }
-    console.log("slides= " + arr);
-    return arr;
-  }
+const handleScroll = (event) => {
+  const index = Math.floor(
+    event.currentTarget.scrollLeft / getSliderItemWidth(),
+  );
+  const indicatorIndex = index / slidesToScroll;
+  // console.log(indicatorIndex, index);
+  Array.from($indicatorList.children).forEach(
+    ($indicatorPoint, indicatorPointIndex) => {
+      $indicatorPoint.classList.toggle(
+        `slider__point_active`,
+        indicatorPointIndex === indicatorIndex,
+      );
+      console.log(indicatorPointIndex, index);
+    },
+  );
 };
+$sliderList.addEventListener("scroll", handleScroll);
+
+// const getActiveIndicators = () => {
+//   console.log("offset= " + getSliderOffset());
+
+//   if (getSliderOffset() !== 0) {
+//     console.log("not zero");
+//     const lastSlide = Math.round(
+//       getSliderOffset() / getSliderItemWidth() + slidesCount,
+//     );
+//     console.log("last slide= " + lastSlide);
+//     const arr = [];
+//     for (
+//       let i = Math.round(getSliderOffset() / getSliderItemWidth());
+//       i < lastSlide;
+//       i++
+//     ) {
+//       arr.push(Math.round(i));
+//     }
+//     console.log("slides= " + arr);
+//     return arr;
+//   } else {
+//     console.log("zero");
+//     const arr = [];
+//     for (let i = 0; i < slidesCount; i++) {
+//       arr.push(Math.round(i));
+//     }
+//     console.log("slides= " + arr);
+//     return arr;
+//   }
+// };
+
 // const updateIndicatorsList = () => {
 //   for (let i = 0; getActiveIndicators().length; i++) {
 //     $indicatorList.children[getActiveIndicators[i]].className.
@@ -96,26 +117,26 @@ const getActiveIndicators = () => {
 // };
 
 // получение индекса слайда, что вошел в область родительского контейнера
-const interObserver = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const index = Array.from($sliderList.children).indexOf(entry.target);
-        // console.log(index + 1);
-        arrayOfIndexSlides.push(index + 1);
-        // console.log(arrayOfIndexSlides);
-      }
-    });
-  },
-  { root: $sliderList, threshold: 0.1, rootMargin: "0px -350px 0px -350px" },
-);
+// const interObserver = new IntersectionObserver(
+//   (entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         const index = Array.from($sliderList.children).indexOf(entry.target);
+//         // console.log(index + 1);
+//         arrayOfIndexSlides.push(index + 1);
+//         // console.log(arrayOfIndexSlides);
+//       }
+//     });
+//   },
+//   { root: $sliderList, threshold: 0.1, rootMargin: "0px -350px 0px -350px" },
+// );
 
-document.querySelectorAll(".slider__item").forEach((slide) => {
-  interObserver.observe(slide);
-});
+// document.querySelectorAll(".slider__item").forEach((slide) => {
+//   interObserver.observe(slide);
+// });
 
-$prevButton.addEventListener("click", handlePrevScroll);
-$nextButton.addEventListener("click", handleNextScroll);
+$prevButton.addEventListener("click", handleMoveToPrev);
+$nextButton.addEventListener("click", handleMoveToNext);
 
 document.addEventListener("DOMContentLoaded", createIndicators);
-document.addEventListener("DOMContentLoaded", getActiveIndicators);
+// document.addEventListener("DOMContentLoaded", getActiveIndicators);
